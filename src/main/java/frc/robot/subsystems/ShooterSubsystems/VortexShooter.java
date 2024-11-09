8c7b0eebaed6faf1ba5d12501e21c9e1cd5dbf98
Jasper Chen <jdchen2017@gmail.com>
2024-11-08 23:44:29 -0600

    vortex shooter imports are wonky

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystems/VortexShooter.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.ShooterSubsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.CANSparkFlex;
+
+public class VortexShooter extends SubsystemBase implements IShooterSubsystem {
+    //private final SparkFlex ShooterMotor 
+
+    @Override
+    public void setShooter(double speed) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setShooter'");
+    }
+
+    @Override
+    public void stopShooter() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stopShooter'");
+    }
+
+    @Override
+    public double getRate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRate'");
+    }
+
+}
