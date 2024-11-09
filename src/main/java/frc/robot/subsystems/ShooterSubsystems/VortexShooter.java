164211f686049dabd18ef3fbd5bf0166c3b3aca7
Jasper Chen <jdchen2017@gmail.com>
2024-11-09 08:24:32 -0600

    vortex shooter

--- src/main/java/frc/robot/subsystems/ShooterSubsystems/VortexShooter.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystems/VortexShooter.java
@@ -1,28 +1,38 @@
 package frc.robot.subsystems.ShooterSubsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class VortexShooter extends SubsystemBase implements IShooterSubsystem {
-    //private final SparkFlex ShooterMotor 
+    private final CANSparkFlex ShooterMotor = new CANSparkFlex(Constants.ShooterMotor,MotorType.kBrushless);
+
+    public VortexShooter(){
+        ShooterMotor.setIdleMode(IdleMode.kCoast);
+        //still need to configure the spark flex's current limits
+    }
 
     @Override
     public void setShooter(double speed) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setShooter'");
+        //check for temperature issues
+        if (ShooterMotor.getMotorTemperature()>=80.0){
+            ShooterMotor.set(0);
+        }else{
+            ShooterMotor.set(speed);
+        }
     }
 
     @Override
     public void stopShooter() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'stopShooter'");
+        ShooterMotor.set(0);
     }
 
     @Override
     public double getRate() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRate'");
+        return -1.0;
     }
 
 }
