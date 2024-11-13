503636c7c1d7e689bb23ee2954877497ebccd119
NJonathanJiang <theNerdyMango@gmail.com>
2024-11-12 21:21:17 -0600

    .

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -11,7 +11,7 @@ public class DriveCommand extends Command {
     private final DriveSubsystem drive;
     private final OI oi = OI.getInstance();
 
-    private final SlewRateLimiter rotationLimiter = new SlewRateLimiter(3);
+    private final SlewRateLimiter rotationLimiter = new SlewRateLimiter(7);
     private final SlewRateLimiter speedLimiter = new SlewRateLimiter(7);
 
     public DriveCommand(DriveSubsystem drive) {
@@ -27,7 +27,12 @@ public class DriveCommand extends Command {
 
     @Override
     public void execute() {
-        drive.arcadeDrive(speedLimiter.calculate(oi.driverController().axis(OI.Axes.LEFT_STICK_Y)) * Constants.DRIVE_SPEED, rotationLimiter.calculate(oi.driverController().axis(OI.Axes.RIGHT_STICK_X)) * Constants.ROTATION_SENSITIVITY);
+        double speed = Math.pow(oi.driverController().axis(OI.Axes.LEFT_STICK_Y), 3.0);
+        double rotate = Math.pow(-oi.driverController().axis(OI.Axes.RIGHT_STICK_X), 3.0);
+        drive.arcadeDrive(
+            speedLimiter.calculate(speed) * Constants.DRIVE_SPEED,
+            rotationLimiter.calculate(rotate) * Constants.ROTATION_SENSITIVITY
+        );
     }
 
     @Override
